60621247a049143fb22ce0fe52c19526b7d59593
wbadmin <>
2023-11-28 12:14:46 +0530

    {/src/main/java/com/jobs/ags/hr/interviewprocess/InterviewDetail.java}

--- src/main/java/com/jobs/ags/hr/interviewprocess/InterviewDetail.java
+++ src/main/java/com/jobs/ags/hr/interviewprocess/InterviewDetail.java
@@ -11,6 +11,10 @@ public class InterviewDetail implements java.io.Serializable {
 	private java.lang.String interviewProcessId;
 	private java.lang.String waitingTime;
 
+	private java.lang.String assesmentTime;
+
+	private java.lang.String status;
+
 	public InterviewDetail() {
 	}
 
@@ -30,10 +34,29 @@ public class InterviewDetail implements java.io.Serializable {
 		this.waitingTime = waitingTime;
 	}
 
+	public java.lang.String getAssesmentTime() {
+		return this.assesmentTime;
+	}
+
+	public void setAssesmentTime(java.lang.String assesmentTime) {
+		this.assesmentTime = assesmentTime;
+	}
+
+	public java.lang.String getStatus() {
+		return this.status;
+	}
+
+	public void setStatus(java.lang.String status) {
+		this.status = status;
+	}
+
 	public InterviewDetail(java.lang.String interviewProcessId,
-			java.lang.String waitingTime) {
+			java.lang.String waitingTime, java.lang.String assesmentTime,
+			java.lang.String status) {
 		this.interviewProcessId = interviewProcessId;
 		this.waitingTime = waitingTime;
+		this.assesmentTime = assesmentTime;
+		this.status = status;
 	}
 
 }
